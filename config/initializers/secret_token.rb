fa6d2d612eaff1d2fac737ca9e1b93b44aa8c046
Tsunadevictoria <tsunadevictoria@hotmail.com>
2013-11-05 17:48:24 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PxL::Application.config.secret_key_base = 'aada5025d17a71a243c184999693ab9d2703ec9e1646a5a6d485620c11af6e14196479716a32db1e8f8679321375827ed9f53299bcc9c46ae74f0c22739d4df1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PxL::Application.config.session_store :cookie_store, key: '_PxL_session'
