9c06451b50b2d8dc97bf4812465840b6ad2fafd1
Tsunadevictoria <tsunadevictoria@hotmail.com>
2013-11-06 17:02:28 -0600

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,6 @@ class Product < ActiveRecord::Base
 	validates :image_url, :allow_blank => true, :format => {
 	    :with    => %r{\.(gif|jpg|png)\Z}i,
 	    :message => 'must be a URL for GIF, JPG or PNG image.'
-  }
+    }
+    validates :title, :length => {:minimum => 10}
 end
