0d01225b2f913c4c28c8a5aadfac3e407fb4eec4
Tsunadevictoria <tsunadevictoria@hotmail.com>
2013-11-07 00:33:51 -0600

    ver bootstrap

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,15 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+#Bundler.require(:default, Rails.env)
+
+
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module PxL
   class Application < Rails::Application
@@ -20,6 +28,12 @@ module PxL
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 	config.assets.initialize_on_precompile = false
+
+	# Enable the asset pipeline
+    config.assets.enabled = true
+
+	# Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
 
 	config.generators do |g|
   		g.assets false
